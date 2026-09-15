06fe5c483f901d3ae5abf40dd78930a2d82eb6fe
MatheusMoreira <matheus.moreira.marcelino.dev@gmail.com>
2026-09-15 00:34:30 -0300

    botao unico de adicionar, excluir e mover

--- scripts/renderizarItensCatalogo.js
+++ scripts/renderizarItensCatalogo.js
@@ -4,27 +4,28 @@ function buildCard(dadosItem) {
   let strHTML;
 
   strHTML = `
-    <div class="col" data-bs-toggle="offcanvas" data-bs-target="#detalhesItem" aria-controls="detalhesItem" style="cursor: pointer;">
-      <div class="card">
-        <div class="card-header d-flex gap-2 align-items-center">
-          <span class="badge bg-secondary border ${!dadosItem.temEstoque ? "d-none" : ""}">${dadosItem.estoque}</span>
-          <h6 class="card-title m-0 text-truncate" title="${dadosItem.nomeItem}">${dadosItem.nomeItem}</h6>
+    <div class="col">
+      <div class="card shadow p-2 p-md-3 rounded-3" data-bs-toggle="offcanvas" data-bs-target="#detalhesItem" aria-controls="detalhesItem" style="cursor: pointer;">
+        <div class="card-header">
+          <h6 class="card-title text-center m-0 text-truncate" title="${dadosItem.nomeItem}">${dadosItem.nomeItem}</h6>
         </div>
-
-        <img src=${dadosItem.imagem} class="card-img-top" alt="Sem Imagem">
-        <div class="card-body"></div>
-
-        <div class="card-footer container">
-          <div class="row gap-1">
-            <button class="col btn btn-outline-success">
-              <i class="bi bi-plus"></i>
-              <span class="d-none d-md-inline">Entrada</span>
-            </button>
-            <button class="col btn btn-outline-danger">
-              <i class="bi bi-x"></i>
-              <span class="d-none d-md-inline">Saída</span>
-            </button>
+        
+        <div 
+          class="position-relative ratio ratio-1x1 w-100 bg-secondary-subtle rounded-3"
+          style="background-image: url(${dadosItem.imagem || ""}); background-size: cover; background-position: center; background-repeat: no-repeat;"
+        >
+          
+          <div>
+            <span class="fs-6 position-absolute bottom-0 start-50 translate-middle-x mb-2 badge bg-info ${!dadosItem.temEstoque ? "d-none" : ""}">
+              ${dadosItem.estoque}
+            </span>
           </div>
+        </div>
+        
+        <div class="card-footer bg-light p-0 mt-auto">
+          <button class="py-md-3 btn btn-outline-secondary w-100 rounded-0 border-0">
+            <i class="bi bi-arrow-left-right"></i>
+          </button>
         </div>
       </div>
     </div>
